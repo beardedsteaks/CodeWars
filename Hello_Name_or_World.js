6cec33ed16d1aa7c968a68579aa178bd54364790
Jason McRainey <beardedsteaks@gmail.com>
2021-01-17 23:55:19 +0900

    Resolved formatting of the name

--- Hello_Name_or_World.js
+++ Hello_Name_or_World.js
@@ -1,14 +1,21 @@
 function hello(name) {
-  let length = name.length;
-  
+	
+  var length = name.length;
+
   if (length > 0){
-	  name[0] = name[0].toUpperCase();
-	  for (let i = 1; i < length; i++) {
-		  name[i] = name[i].toLowerCase();
-	  }
-	  let message = 'Hello, ' + name + '!'
-	  return message;
-  } else {
-	  return 'Hello, World!';
+
+    var fname = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+    console.log(fname)
+
+    let message = 'Hello, ' + fname + '!'
+	
+    return message;
+
+  } else if (length === 0) {
+	  
+    return 'Hello, World!';
+
   }
+
 }
