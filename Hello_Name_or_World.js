1d5f5fe15aa9d43827a4539c8a45541764e215b4
Jason McRainey <beardedsteaks@gmail.com>
2021-01-18 20:06:05 +0900

    Check for NULL value included
    This build was submitted as my solution to this Code War Challenge

--- Hello_Name_or_World.js
+++ Hello_Name_or_World.js
@@ -1,21 +1,26 @@
 function hello(name) {
+
+	if (name == null) {
+		return 'Hello, World!';
 	
-  var length = name.length;
-
-  if (length > 0){
-
-    var fname = name[0].toUpperCase() + name.slice(1).toLowerCase();
-
-    console.log(fname)
-
-    let message = 'Hello, ' + fname + '!'
+	}
 	
-    return message;
+	var length = name.length;
 
-  } else if (length === 0) {
+  	if (length > 1){
+
+    	var fname = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+    	console.log(fname)
+
+    	let message = 'Hello, ' + fname + '!'
+	
+    	return message;
+
+  	} else if (length < 2) {
 	  
-    return 'Hello, World!';
+    	return 'Hello, World!';
 
-  }
+  	}
 
 }
