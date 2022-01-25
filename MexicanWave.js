a4ea8dd3a34ba1aca331081066ab2aa8d13297cc
Jason McRainey <beardedsteaks@gmail.com>
2022-01-25 21:55:32 +0900

    removed unneccessary comments from Mexican Wave

--- MexicanWave.js
+++ MexicanWave.js
@@ -26,27 +26,6 @@ function wave(str){
 
 //Tests
 
-/*
-    describe("Tests", () => {
-    it("test", () => {
-    result = ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
-    Test.assertDeepEquals(wave("hello"),result, "Should return: '"+result+"'");
-
-    result = ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"];
-    Test.assertDeepEquals(wave("codewars"), result, "Should return: '"+result+"'");
-
-    result = [];
-    Test.assertDeepEquals(wave(""), result, "Should return: '"+result+"'");
-
-    result = ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"];
-    Test.assertDeepEquals(wave("two words"), result, "Should return: '"+result+"'");
-
-    result = [" Gap ", " gAp ", " gaP "];
-    Test.assertDeepEquals(wave(" gap "), result, "Should return: '"+result+"'");
-    });
-    });
-
-*/
 
 wave('hello');
 wave();
